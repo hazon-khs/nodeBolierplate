ee07bb4ea1ab143b30c37df22ab8c922a4fd961a
김현수 <anikka@gimhyeonsuui-MacBookAir.local>
2021-11-07 16:58:11 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://anikka:anikka123@cluster0.8dzsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+app.use(express.urlencoded({extends:true}));
+app.use(express.json());
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=>{
     console.log("MongoDB Connected Success!!")
 }).catch(err=>{
     console.log(err)
-})
+});
 
-app.get("/", (req, res) => res.send("Hello world!!"))
-app.listen(port, () => console.log(`Example app listing port : ${port}!`))
+app.get("/", (req, res) => res.send("Hello world!! Come On!!"));
+app.post("/register", (req, res)=>{
+    //회원가입할때 필요한 정보들을 클라이언트에서 가져오면
+    //그것들을 데이터 베이스에 넣어 준다.
+
+    const user = new User(req.body)
+
+    user.save((err, doc)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({success:true})
+    })
+
+});
+app.listen(port, () => console.log(`Example app listing port : ${port}!`));

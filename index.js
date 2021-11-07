ba6c202a3c76b57d56209783c4e0b53d22dd2dcd
김현수 <anikka@gimhyeonsuui-MacBookAir.local>
2021-11-07 18:44:13 +0900

    로그인 기능추가

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { User } = require("./models/User");
+const cookie = require('cookie-parser');
 
 app.use(express.urlencoded({extends:true}));
 app.use(express.json());
-
+app.use(cookie());
 const config = require('./config/key');
 
 const mongoose = require('mongoose');
@@ -22,12 +23,38 @@ app.post("/register", (req, res)=>{
     //회원가입할때 필요한 정보들을 클라이언트에서 가져오면
     //그것들을 데이터 베이스에 넣어 준다.
 
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     user.save((err, doc)=>{
         if(err) return res.json({success:false, err})
         return res.status(200).json({success:true})
-    })
+    });
 
 });
+
+app.post('/login', (req, res)=>{
+
+    console.log(req.body);
+    //요청된 이메일을 데이터 베이스에서 찾는다.
+    User.findOne({email:req.body.email}, (err, userInfo)=>{
+       if(!userInfo) return res.json({loginSuccess:false,message:"유저가 존재하지 않습니다."}); 
+
+            //비밀번호가 동일한지 체크한다.
+            userInfo.comparePassword(req.body.password, (err, isMatch)=>{
+                console.log(isMatch);
+                if(!isMatch) return res.json({loginSuccess:false, message:"비밀번호가 일치하지 않습니다."});
+
+                userInfo.generateToken((err, user)=>{
+                    if(err) return res.status(400).send(err);
+
+                    //생성된 토큰을 저장합니다.
+                    res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({loginSuccess:true, userId: user._id});
+                });
+            });
+    });
+
+});
+
 app.listen(port, () => console.log(`Example app listing port : ${port}!`));
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -10,7 +13,7 @@ const userSchema = mongoose.Schema({
         trim:true,
         unique:1
     },
-    passeord:{
+    password:{
         type:String,
         minlength:5
     },
@@ -33,6 +36,46 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre('save', function(next){
+
+    var user = this;
+
+    if(user.isModified('password')){
+
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+            
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword
+    console.log(`plainPassword : ${plainPassword} , thisPassword : ${this.password}`);
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        return cb(null, isMatch);
+    });   
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'anikkaToken');
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    });
+}
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
